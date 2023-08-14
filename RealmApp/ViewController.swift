e4f85e940910c2f9d7824ce30ba91ecf0006e85f
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-08-14 16:40:22 +0300

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by Shamkhan Mutuskhanov on 14.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
